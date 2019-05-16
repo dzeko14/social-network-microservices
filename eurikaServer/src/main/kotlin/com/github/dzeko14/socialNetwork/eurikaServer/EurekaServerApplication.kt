b873aed2bab0f5cb71e783f1ac9b6bbe8a9f0780
Евгений <dzekomerden@gmail.com>
2019-05-16 20:26:21 +0300

    Add eureka server. Make userService as client of discovery server

---
+++ eurikaServer/src/main/kotlin/com/github/dzeko14/socialNetwork/eurikaServer/EurekaServerApplication.kt
@@ -0,0 +1,13 @@
+package com.github.dzeko14.socialNetwork.eurikaServer
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer
+
+@EnableEurekaServer
+@SpringBootApplication
+class EurekaServerApplication
+
+fun main(args: Array<String>) {
+	runApplication<EurekaServerApplication>(*args)
+}
--- userService/src/main/kotlin/com/github/dzeko14/socialNetwork/userService/UserServiceApplication.kt
+++ userService/src/main/kotlin/com/github/dzeko14/socialNetwork/userService/UserServiceApplication.kt
@@ -2,7 +2,9 @@ package com.github.dzeko14.socialNetwork.userService
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient
 
+@EnableEurekaClient
 @SpringBootApplication
 class UserServiceApplication
 
